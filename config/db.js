522bbf136cb3c3aada0b8dfa6ea9634e73f18893
bandarAmed <bandarAmed>
2024-02-04 13:32:39 +0300

    photogram

--- config/db.js
+++ config/db.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 
 // connect in mongoose datapase but useing .env
 async function db() {
+    mongoose.set({strictQuery: false})
     try {
-        await mongoose.connect(process.env.DB_URL).then(console.log('Connected on mongoose database'))
+        await mongoose.connect(process.env.DB_URL);
+        console.log('Connected on mongoose database')
     } catch (e) {
         console.log(e)
     }
